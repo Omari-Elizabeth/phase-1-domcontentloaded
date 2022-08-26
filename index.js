2887796d5f31a7e37fa3f61654227dc18e6b0459
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-26 13:48:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let text = document.querySelector("p").textContent = "This is really cool!"
+    
+  });
